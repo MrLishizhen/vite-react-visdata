ffab1bb79907f9aed9b1f9ce92922e81c1e3e8ab
Mrlishizhen <718647063@qq.com>
2023-04-11 17:40:38 +0800

    fix:修改title以及增加导航栏内容为空

--- src/components/menus/index.tsx
+++ src/components/menus/index.tsx
@@ -1,71 +1,96 @@
-import styles from './index.module.less'
-import {ChangeEvent, useEffect, useState} from "react";
-import {list} from '../../router/list'
-import {useNavigate, useLocation} from 'react-router-dom'
-import screenfull from 'screenfull'
+import styles from "./index.module.less";
+import { ChangeEvent, useEffect, useState } from "react";
+import { list } from "../../router/list";
+import { useNavigate, useLocation } from "react-router-dom";
+import screenfull from "screenfull";
 
 const Menus = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const [menu_list, set_menu_list] = useState(list)
-    const [path_name, set_path_name] = useState('')
-    const [full, set_full] = useState<boolean>(true)
-    const [btn_state, set_ben_state] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [menu_list, set_menu_list] = useState(list);
+  const [path_name, set_path_name] = useState("");
+  const [full, set_full] = useState<boolean>(true);
+  const [btn_state, set_ben_state] = useState<boolean>(false);
 
-    /*判断路由修改匹配对应menu_item*/
-    useEffect(() => {
-        set_path_name(location.pathname.slice(1))
-    }, [location])
-    /*点击隐藏导航栏*/
-    const btn_click = () => {
-        set_ben_state(!btn_state)
-    }
-    /*导航跳转*/
-    const to_path = (link: string) => {
-        navigate(`/${link}`)
-    }
-    /*点击全屏*/
-    const full_click = () => {
-        screenfull.toggle();
-        set_full(false)
-    }
-    /*导航查找*/
-    const search = (value: string) => {
-        set_menu_list(list.filter(u => u.name.indexOf(value) > -1))
-    }
-    /*sereenfull change事件*/
-    useEffect(() => {
-        const change = function () {
-            set_full(!screenfull.isFullscreen)
-        }
-        screenfull.on('change', change)
-        return () => {
-            screenfull.off('change', change);
-        }
-    }, [])
-    return full ? <div className={`${styles.menus} ${btn_state ? styles.menus_hot : ''}`}>
+  /*判断路由修改匹配对应menu_item*/
+  useEffect(() => {
+    set_path_name(location.pathname.slice(1));
+  }, [location]);
+  /*点击隐藏导航栏*/
+  const btn_click = () => {
+    set_ben_state(!btn_state);
+  };
+  /*导航跳转*/
+  const to_path = (link: string) => {
+    navigate(`/${link}`);
+  };
+  /*点击全屏*/
+  const full_click = () => {
+    screenfull.toggle();
+    set_full(false);
+  };
+  /*导航查找*/
+  const search = (value: string) => {
+    set_menu_list(list.filter((u) => u.name.indexOf(value) > -1));
+  };
 
-        <div className={styles.search}>
-            <input type="text" placeholder={'查找'} onChange={(event) => search(event.target.value)}/>
-        </div>
-        <div className={styles.icons}>
-            <div className={`${styles.btn_icon_full}`} onClick={full_click}/>
-            <div className={`${styles.btn_icon} ${btn_state ? styles.hot : ''}`} onClick={btn_click}/>
-        </div>
-        <ul className={styles.menus_ul}>
-            {
-                menu_list.map(u => {
-                    return (<li key={u.path} onClick={() => to_path(u.path)}
-                                className={`${styles.menus_li} ${path_name === u.path ? styles.hot : ''}`}>
-                        {
-                            u.name
-                        }
-                    </li>)
-                })
-            }
+  useEffect(() => {
+    let { name } = menu_list[0] || {};
+    document.title = name;
+  }, [menu_list]);
 
-        </ul>
-    </div> : <></>
-}
+  /*sereenfull change事件*/
+  useEffect(() => {
+    const change = function () {
+      set_full(!screenfull.isFullscreen);
+    };
+    screenfull.on("change", change);
+    return () => {
+      screenfull.off("change", change);
+    };
+  }, []);
+  return full ? (
+    <div className={`${styles.menus} ${btn_state ? styles.menus_hot : ""}`}>
+      <div className={styles.search}>
+        <input
+          type="text"
+          placeholder={"查找"}
+          onChange={(event) => search(event.target.value)}
+        />
+      </div>
+      <div className={styles.icons}>
+        <div className={`${styles.btn_icon_full}`} onClick={full_click} />
+        <div
+          className={`${styles.btn_icon} ${btn_state ? styles.hot : ""}`}
+          onClick={btn_click}
+        />
+      </div>
+      <ul className={styles.menus_ul}>
+        {menu_list.map((u) => {
+          return (
+            <li
+              key={u.path}
+              onClick={() => to_path(u.path)}
+              className={`${styles.menus_li} ${
+                path_name === u.path ? styles.hot : ""
+              }`}
+            >
+              {u.name}
+            </li>
+          );
+        })}
+        {menu_list.length === 0 ? (
+          <li style={{ textAlign: "center", color: "#ccc", fontSize: 14 }}>
+            暂无更多
+          </li>
+        ) : (
+          ""
+        )}
+      </ul>
+    </div>
+  ) : (
+    <></>
+  );
+};
 
-export default Menus
+export default Menus;
